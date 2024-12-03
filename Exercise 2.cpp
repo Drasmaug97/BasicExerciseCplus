5ca9d803695b2e2538e563622d4534e0ba3d9f62
Damián <damiramos.r10@gmail.com>
2024-12-02 21:20:14 -0300

    Update and rename Ejercicio 2.cpp to Exercise 2.cpp

--- Exercise 2.cpp
+++ Exercise 2.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
     /*
-    Hacer un programa que solicite por teclado que se ingresen dos números y luego guardarlos en dos variables distintas.
-    A continuación se deben intercambiar mutuamente los valores en ambas variables y mostrarlos por pantalla.
+    Hacer un programa que solicite por teclado que se ingresen dos nÃºmeros y luego guardarlos en dos variables distintas.
+    A continuaciÃ³n se deben intercambiar mutuamente los valores en ambas variables y mostrarlos por pantalla.
     Ejemplo: Suponiendo que se ingresan 3 y 8 como valores
     y que la variables usadas son A y B, entonces A=3 y B=8, pero luego debe quedar A=8 y B=3.
     */
@@ -20,7 +20,7 @@ int main (){
     n2 = n1;
     n1 = aux;
 
-    cout << "Invirtiendo los numero sería: " << endl;
+    cout << "Invirtiendo los numero serÃ­a: " << endl;
     cout << "Primer numero " << n1 << endl;
     cout << "Segundo numero " << n2;
 
