ff70aebbd7ceaeffac62ab865e07b0435db93806
Damián <damiramos.r10@gmail.com>
2024-12-02 21:21:08 -0300

    Update and rename Ejercicio 5.cpp to Exercise 5.cpp

--- Exercise 5.cpp
+++ Exercise 5.cpp
@@ -4,7 +4,7 @@ using namespace std;
     /*Un comercio vende tres marcas de alfajores distintas A, B y C.
     Hacer un programa para ingresar por teclado la cantidad de alfajores vendidos de cada una de las tres marcas
     y luego se informe el porcentaje de ventas para cada una de ellas.
-    Ejemplo. Si se ingresa 100, 25 y 75 como cantidades vendidas entonces el programa calcular· e informar· A: 50%, B: 12,50% y C: 37,50%.
+    Ejemplo. Si se ingresa 100, 25 y 75 como cantidades vendidas entonces el programa calcular√° e informar√° A: 50%, B: 12,50% y C: 37,50%.
     */
 
 int main (){
